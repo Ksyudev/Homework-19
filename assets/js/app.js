331e49b79fdb762231516b67460f81b6cb794592
Ksyudev <92163392+Ksyudev@users.noreply.github.com>
2021-11-24 20:47:07 +0200

    with diagnosis

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,16 @@ function index()
     else */let index = mass/(height**2);
  index = Math.floor(index*100)/100;
  resultPlace.innerHTML = index;
+ if ( index <=18.5 )
+    {diagnosisPlace.innerHTML = 'Ваша масса тела недостаточная';}
+else if ((index>=18.5) && (index<25))
+    {diagnosisPlace.innerHTML = 'Ваша масса тела нормальная';}
+else if ((index>=25) && (index<30))
+    {diagnosisPlace.innerHTML = 'Ваша масса тела избыточная';}
+else if ((index>=30) && (index<35))
+    {diagnosisPlace.innerHTML = 'У Вас ожирение 1 степени';}
+else if ((index>=35) && (index<40))
+    {diagnosisPlace.innerHTML = 'У Вас ожирение 2 степени';}
+else if (index>=40)
+    {diagnosisPlace.innerHTML = 'У Вас ожирение 3 степени';}
 }
