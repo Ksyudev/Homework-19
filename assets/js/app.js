5dbec9e8782e737d072443e05410f6986f0e733c
Ksyudev <92163392+Ksyudev@users.noreply.github.com>
2021-11-25 10:53:14 +0200

    ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
-
-function index()
+function BMI()
 {   let height =+heightInput.value;
+    if (height>3) 
+    {height=height/100;} 
     console.log(height);
     let mass = +massInput.value;
     console.log(mass);
-   /* if (height>3) 
-    {let index = mass/((height/100)**2);}
-    else */let index = mass/(height**2);
- index = Math.floor(index*100)/100;
- resultPlace.innerHTML = index;
+    let index = mass/(height**2)
+    index = Math.floor(index*100)/100;
+    resultPlace.innerHTML = index;
+    console.log(index);
  if ( index <=18.5 )
     {diagnosisPlace.innerHTML = 'Ваша масса тела недостаточная';}
 else if ((index>=18.5) && (index<25))
@@ -21,4 +21,7 @@ else if ((index>=35) && (index<40))
     {diagnosisPlace.innerHTML = 'У Вас ожирение 2 степени';}
 else if (index>=40)
     {diagnosisPlace.innerHTML = 'У Вас ожирение 3 степени';}
+if (height==0 || mass==0)
+    {resultPlace.innerHTML = '';
+    diagnosisPlace.innerHTML = 'Введите ваш рост и вес';} 
 }
